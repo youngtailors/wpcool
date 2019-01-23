525f9ffdb8e9d3f0ba8e57a679c56d89e1f1b4a1
Mark Allen <doantufptu@gmail.com>
2019-01-23 14:52:44 +0700

    [wip]

--- src/modules/post/resolver.ts
+++ src/modules/post/resolver.ts
@@ -19,8 +19,55 @@ export class PostResolver {
       const result = await axios.get(
         `https://public-api.wordpress.com/rest/v1.1/sites/${site}/posts/${ID}`,
       )
-      const post = result.data
-      return camelizeKeys(post)
+      const post = camelizeKeys(result.data)
+      return post
+    } catch (error) {
+      throw error
+    }
+  }
+
+  @Query(() => [Post], { nullable: true })
+  async posts(@Args()
+  {
+    siteID,
+    siteURL,
+    number,
+    order,
+    orderBy,
+    category,
+    author,
+    search,
+  }: PostByIDArgs) {
+    if (!siteID && !siteURL) {
+      throw new Error('Must provide siteID or siteURL')
+    }
+    if (siteID && siteURL) {
+      throw new Error('Must provide only siteID or siteURL')
+    }
+    const site = siteID || siteURL.replace(/(http|https):\/\//, '')
+    // const args = {number, order, orderBy, category, author, search}
+    // let params = ''
+    // Object.keys(args).forEach(key => {
+    //   if (args[key]) {
+    //     params += '?' + decamelizeKeys(key) + '=' + args[key]
+    //   }
+    // })
+    try {
+      const result = await axios.get(
+        `https://public-api.wordpress.com/rest/v1.1/sites/${site}/posts`,
+        {
+          params: {
+            number,
+            order,
+            orderBy,
+            category,
+            author,
+            search,
+          },
+        },
+      )
+      const posts = camelizeKeys(result.data.posts)
+      return posts
     } catch (error) {
       throw error
     }
--- src/types/Common.ts
+++ src/types/Common.ts
@@ -6,4 +6,16 @@ export class Site {
   siteID: number
   @Field(() => String, { nullable: true })
   siteURL: string
+  @Field(() => Int, { nullable: true })
+  number: number
+  @Field(() => String, { nullable: true })
+  order: string
+  @Field(() => String, { nullable: true })
+  orderBy: string
+  @Field(() => String, { nullable: true })
+  category: string
+  @Field(() => Int, { nullable: true })
+  author: number
+  @Field(() => String, { nullable: true })
+  search: string
 }
