5824b23e2654b4b367451b9d2cccae9ffecbfa10
PhongPV <phamvanphong915@gmail.com>
2019-02-11 11:50:55 +0700

    Add utils file

--- src/modules/post/resolver.ts
+++ src/modules/post/resolver.ts
@@ -2,7 +2,7 @@ import { Resolver, Query, Args } from 'type-graphql'
 import { Post } from '../../entities/Post'
 import { PostByIDArgs } from './types'
 import { getPostById } from '../../services/wp'
-import { camelizeKeys } from 'humps'
+import { camelize } from '../../utils/camel'
 
 @Resolver(Post)
 export class PostResolver {
@@ -23,7 +23,7 @@ export class PostResolver {
     try {
       const result = await getPostById(site, ID, slug)
       const post = result.data
-      return camelizeKeys(post)
+      return camelize(post)
     } catch (error) {
       throw error
     }
