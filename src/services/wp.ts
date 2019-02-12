8aadfb3a3656663237d823ba470752e45873eb84
PhongPV <phamvanphong915@gmail.com>
2019-02-12 10:19:48 +0700

    Resolve issue Closes #12

--- src/services/wp.ts
+++ src/services/wp.ts
@@ -7,12 +7,29 @@ const api = axios.create({
     data => {
       const newData = JSON.parse(data)
       Object.keys(newData).map(item => {
-        if (!(typeof newData[item] === dataPost[item])) {
-          if (dataPost[item] === 'string') {
-            newData[item] = newData[item] + ''
+        if (dataPost[item] === 'array') {
+          if (!Array.isArray(newData[item])) {
+            if (typeof newData[item] === 'string') {
+              newData[item] = [newData[item]]
+            }
+            if (typeof newData[item] === 'boolean') {
+              newData[item] = []
+            }
+            if (typeof newData[item] === 'object') {
+              Object.keys(newData[item]).map(i => newData[item][i])
+            }
           }
-          if (dataPost[item] === 'boolean') {
-            newData[item] = false
+        } else {
+          if (!(typeof newData[item] === dataPost[item])) {
+            if (dataPost[item] === 'string') {
+              newData[item] = newData[item] + ''
+            }
+            if (dataPost[item] === 'boolean') {
+              newData[item] = false
+            }
+            if (dataPost[item] === 'object') {
+              newData[item] = { [item]: newData[item] }
+            }
           }
         }
       })
